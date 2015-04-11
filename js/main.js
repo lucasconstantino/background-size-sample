a89cf9547e9e71c42dc4807430511108e5167c4d
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2015-04-10 22:55:57 -0300

    Single fuc*** commit.

---
+++ js/main.js
@@ -0,0 +1,58 @@
+/**
+ * @file
+ */
+
+;(function (undefined) {
+
+var app = angular.module('app', []);
+
+/**
+ * Main controller.
+ */
+app.controller('BackgroundSizeCtrl', function ($scope) {
+
+  $scope.backgroundSizes = [
+    'cover',
+    'contain',
+    'auto',
+    '100% auto',
+    '50% auto',
+    'auto 100%',
+    'auto 50%',
+    '100% 100%',
+    '50% 50%',
+    '100% 50%',
+    '50% 100%'
+  ];
+  $scope.backgroundRepeat = ['no-repeat', 'repeat-x', 'repeat-y', 'repeat'];
+  $scope.backgroundPosition = [
+    'left top',
+    'left center',
+    'left bottom',
+    'right top',
+    'right center',
+    'right bottom',
+    'center top',
+    'center center',
+    'center bottom'
+  ];
+
+  $scope.styles = {
+    'background-size': 'cover',
+    'background-repeat': 'no-repeat',
+    'background-position': 'center center'
+  };
+
+  $scope.height = 200;
+  $scope.image = 'http://lorempixel.com/900/500/nature';
+
+  $scope.$watch('height', function (value) {
+    $scope.styles.height = value + 'px';
+  });
+
+  $scope.$watch('image', function (value) {
+    $scope.styles['background-image'] = 'url("' + value + '")';
+  });
+});
+})();
+
